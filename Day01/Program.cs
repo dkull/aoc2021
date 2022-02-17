e34927f9f16baa704ffc99b000fcfdcde8593247
Tanel Liiv <204019+dkull@users.noreply.github.com>
2022-02-17 16:11:27 +0200

    Part1

---
+++ Day01/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+namespace Day01;
+
+public class Day01 {
+    public static void Main(string[] args) {
+        Stopwatch sw = Stopwatch.StartNew();
+
+        string input = File.ReadAllText(args[0]);
+
+        int[] inp = input.Split('\n')
+            .Where(x => x.Length > 0)
+            .Select(x => int.Parse(x))
+            .ToArray();
+
+        var pairs = inp.Zip(inp[1..]);
+
+        int result = pairs.Count(x => x.First < x.Second);
+
+        Console.WriteLine($"Result: {result} in {sw.ElapsedMilliseconds}ms");
+    }
+}
