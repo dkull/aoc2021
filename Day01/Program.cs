ebfb21f80abe247b0a5ba5d75fd2a3d2ae3a8980
Tanel Liiv <204019+dkull@users.noreply.github.com>
2022-02-18 15:44:42 +0200

    Day 01 Part 2

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -4,8 +4,10 @@ using System.Diagnostics;
 
 namespace Day01;
 
-public class Day01 {
-    public static void Main(string[] args) {
+public class Program
+{
+    public static void Main(string[] args)
+    {
         Stopwatch sw = Stopwatch.StartNew();
 
         string input = File.ReadAllText(args[0]);
@@ -15,10 +17,31 @@ public class Day01 {
             .Select(x => int.Parse(x))
             .ToArray();
 
-        var pairs = inp.Zip(inp[1..]);
+        int p1Result = CalcResult(inp, 1);
+        int p2Result = CalcResult(inp, 3);
+        Console.WriteLine($"P1 result: {p1Result} P2 result: {p2Result} in {sw.ElapsedMilliseconds}ms");
+    }
 
-        int result = pairs.Count(x => x.First < x.Second);
+    public static int CalcResult(IEnumerable<int> data, int windowSize)
+    {
+        int[] summed = CollectionToWindowed(data, windowSize)
+            .Select(window => window.Sum()).ToArray();
+        return summed.Zip(summed[1..])
+            .Count(x => x.First < x.Second);
+    }
 
-        Console.WriteLine($"Result: {result} in {sw.ElapsedMilliseconds}ms");
+    public static List<List<int>> CollectionToWindowed(IEnumerable<int> _data, int windowSize)
+    {
+        int[] data = _data.ToArray();
+        int elemCount = data.Length;
+
+        List<List<int>> result = new();
+        for (int i = 0; i < (elemCount - windowSize + 1); i++)
+        {
+            List<int> currentWindow = data[i..(i+windowSize)].ToList();
+            result.Add(currentWindow);
+        }
+
+        return result;
     }
 }
