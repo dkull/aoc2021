cb6f2867b5c8260f2028c7f27576ececad1aca0f
Tanel Liiv <204019+dkull@users.noreply.github.com>
2022-04-08 22:35:34 +0300

    D5P1

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
 
-public class LineDrawer
+public class Line
 {
     int _fromX;
     int _fromY;
@@ -12,13 +12,20 @@ public class LineDrawer
     public int ToX { get; set; }
     public int ToY { get; set; }
 
-    public LineDrawer(string rawLine)
+    public Line(string rawLine)
     {
         string[] tokens = rawLine.Split(" -> ");
 
         var fromCoords = ParseCoords(tokens[0]);
         var toCoords = ParseCoords(tokens[1]);
-        Console.WriteLine(fromCoords);
+
+        // make all lines left-to-right, top-to-bottom
+        if ((fromCoords.X > toCoords.X) || (fromCoords.Y > toCoords.Y))
+        {
+            (fromCoords, toCoords) = (toCoords, fromCoords);
+        }
+
+        Console.WriteLine($"NewLine: {fromCoords} -> {toCoords}");
 
         FromX = fromCoords.X;
         FromY = fromCoords.Y;
@@ -38,6 +45,44 @@ public class LineDrawer
     public bool IsStraight()
     {
         return FromX == ToX || FromY == ToY;
+    }
+
+    public (int X, int Y)[] overlappers(Line other)
+    {
+        var mycoords = coveredCoordinates();
+        var othercoords = other.coveredCoordinates();
+        /*Console.WriteLine("===========");
+        mycoords.ToList().ForEach(x => Console.WriteLine($"MY --> {x}"));
+        othercoords.ToList().ForEach(x => Console.WriteLine($"OT --> {x}"));
+        othercoords.ToList().ForEach(x => Console.WriteLine($" --> {x}"));*/
+
+        return mycoords.Intersect(othercoords).ToArray();
+    }
+
+    private (int X, int Y)[] coveredCoordinates()
+    {
+        var coords = new List<(int X, int Y)>();
+        if (this.FromX < this.ToX)
+        {
+            var xAt = this.FromX;
+            while (xAt <= this.ToX)
+            {
+                coords.Add((xAt, this.ToY));
+                xAt += 1;
+            }
+        }
+        if (this.FromY < this.ToY)
+        {
+            var yAt = this.FromY;
+            while (yAt <= this.ToY)
+            {
+                coords.Add((this.ToX, yAt));
+                yAt += 1;
+            }
+        }
+        //Console.WriteLine($"{this} contains");
+        //coords.ForEach(x => Console.WriteLine($"--> {x}"));
+        return coords.ToArray();
     }
 
     public override string ToString()
@@ -64,22 +109,31 @@ class Program
         Console.WriteLine($"P1: {p1} P2: {p2} in {sw.ElapsedMilliseconds} ms");
     }
 
-    static LineDrawer[] LoadData(string filepath)
+    static Line[] LoadData(string filepath)
     {
         string[] inputLines = File.ReadAllLines(filepath);
 
         return inputLines
-            .Select(line => new LineDrawer(line))
+            .Select(line => new Line(line))
             .ToArray();
     }
 
-    static int Part1(LineDrawer[] lines)
+    static int Part1(Line[] lines)
     {
-        foreach (var line in lines)
+        var overlaps = new HashSet<(int, int)>();
+        foreach (var line_a in lines)
         {
-            Console.WriteLine(line);
+            foreach (var line_b in lines)
+            {
+                if (line_a == line_b) continue;
+                var overlappingCoords = line_a.overlappers(line_b);
+                foreach (var overlapper in overlappingCoords)
+                {
+                    overlaps.Add(overlapper);
+                }
+            }
         }
-        return 1;
+        return overlaps.Count();
     }
 
     static int Part2(int i)
