47bb2a0788a36698929cf806b0ed17bf18d21f79
Tanel Liiv <204019+dkull@users.noreply.github.com>
2022-04-13 23:14:34 +0300

    D9P2

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -10,9 +10,9 @@ class Program
         var p1 = P1(data);
         Console.WriteLine($"P1: {p1} in {sw.ElapsedMilliseconds} ms");
 
-        //sw = Stopwatch.StartNew();
-        //var p2 = P2(data);
-        //Console.WriteLine($"P2: {p2} in {sw.ElapsedMilliseconds} ms");
+        sw = Stopwatch.StartNew();
+        var p2 = P2(data);
+        Console.WriteLine($"P2: {p2} in {sw.ElapsedMilliseconds} ms");
     }
 
     static int[][] LoadData(string filepath)
@@ -62,9 +62,35 @@ class Program
         return result;
     }
 
-    public static int P2(List<string> data)
+    private static int fillBasin(int[][] data, int x, int y)
     {
-        var result = 0;
-        return result;
+        try { _ = data[y][x]; } catch { return 0; }
+        if (data[y][x] == 9) return 0;
+
+        data[y][x] = 9; // strike this tile off so others wouldn't count it
+        return 1
+            + fillBasin(data, x, y - 1)
+            + fillBasin(data, x, y + 1)
+            + fillBasin(data, x - 1, y)
+            + fillBasin(data, x + 1, y);
+    }
+
+    public static int P2(int[][] data)
+    {
+        var basinSizes = new List<int>();
+
+        for (var y = 0; y < data.Length; y++)
+        {
+            for (var x = 0; x < data[y].Length; x++)
+            {
+                var basinSize = fillBasin(data, x, y);
+                if (basinSize > 0) basinSizes.Add(basinSize);
+            }
+        }
+
+        basinSizes.Sort();
+        var lastThree = basinSizes.ToArray()[^3..];
+        var product = lastThree.Aggregate((x, y) => x * y);
+        return product;
     }
 }
