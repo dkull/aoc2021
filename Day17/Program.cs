41dca5b5bd4351ec492fce88f2512fcd0d0796fd
Tanel Liiv <204019+dkull@users.noreply.github.com>
2022-04-26 11:21:46 +0300

    D17P2

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -13,10 +13,10 @@ namespace App
             var p1 = P1(data);
             Console.WriteLine($"P1: {p1} in {sw.ElapsedMilliseconds} ms");
 
-            /*data = LoadData(args[0]);
+            data = LoadData(args[0]);
             sw = Stopwatch.StartNew();
             var p2 = P2(data);
-            Console.WriteLine($"P2: {p2} in {sw.ElapsedMilliseconds} ms");*/
+            Console.WriteLine($"P2: {p2} in {sw.ElapsedMilliseconds} ms");
         }
 
         static (int x1, int x2, int y1, int y2) LoadData(string filepath)
@@ -62,7 +62,6 @@ namespace App
             {
                 for (var y = target.y1; y < 1000; y++)
                 {
-                    Console.WriteLine($"== testing {x} {y} (highest {highestY}) ==");
                     (int x, int y) pos = (0, 0);
                     (int x, int y) velocity = (x, y);
 
@@ -90,9 +89,33 @@ namespace App
             P2
         */
 
-        public static long P2((int x1, int x2, int y1, int y2) data)
+        public static long P2((int x1, int x2, int y1, int y2) target)
         {
-            return -1;
+            // this logic doesn't catch all possible cases
+            // we need to start to the left+above the target
+            var goodPos = 0;
+            for (var x = 0; x <= target.x2; x++)
+            {
+                for (var y = target.y1; y < 1000; y++)
+                {
+                    (int x, int y) pos = (0, 0);
+                    (int x, int y) velocity = (x, y);
+
+                    for (var loop = 0;; loop++)
+                    {
+                        if (pos.y < target.y1) break;
+
+                        if (inTargetArea(pos, target))
+                        {
+                            goodPos++;
+                            break;
+                        } else {
+                            step(ref pos.x, ref pos.y, ref velocity.x, ref velocity.y);
+                        }
+                    }
+                }
+            }
+            return goodPos;
         }
     }
 }
