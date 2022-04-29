9d16efb8475330cd4d30cab8f43e5facd2e2c45a
Tanel Liiv <204019+dkull@users.noreply.github.com>
2022-04-29 15:06:46 +0300

    D21P2

--- Day21/Program.cs
+++ Day21/Program.cs
@@ -1,17 +1,20 @@
-﻿using System.Linq;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 
 class Dice
 {
     int value = 0;
     int rolls = 0;
+    int max;
 
-    public Dice() {}
+    public Dice(int max)
+    {
+        this.max = max;
+    }
 
     public int get()
     {
         rolls++;
-        value = (value % 100) + 1;
+        value = (value % max) + 1;
         return value;
     }
 
@@ -50,7 +53,7 @@ class Program
 
     public static long P1((int p1, int p2) players)
     {
-        var dice = new Dice();
+        var dice = new Dice(100);
         var p1score = 0;
         var p2score = 0;
         var p1at = players.p1 - 1;
@@ -60,11 +63,13 @@ class Program
             var rollSum = Enumerable.Range(0, 3).Select(_x => dice.get()).Sum();
             p1at = (p1at + rollSum) % 10;
             p1score += p1at + 1;
+            //Console.WriteLine($"p1: {p1score}");
             if (Math.Max(p1score, p2score) >= 1000) break;
 
             rollSum = Enumerable.Range(0, 3).Select(_x => dice.get()).Sum();
             p2at = (p2at + rollSum) % 10;
             p2score += p2at + 1;
+            //Console.WriteLine($"p2: {p2score}");
             if (Math.Max(p1score, p2score) >= 1000) break;
         }
 
@@ -74,12 +79,59 @@ class Program
         return dicerolls * minScore;
     }
 
+
+    public static (long p1, long p2) game(Dictionary<int, int> map, int _universes, int _p1score, int _p2score, int _p1at, int _p2at, bool p1turn)
+    {
+        if (_p1score >= 21) return (1, 0);
+        if (_p2score >= 21) return (0, 1);
+
+        var wins = (p1: (long)0, p2: (long)0);
+        foreach (var (rollSum, universes) in map)
+        {
+            var pat = p1turn ? _p1at : _p2at;
+            var pscore = p1turn ? _p1score : _p2score;
+
+            pat = (pat + rollSum) % 10;
+            pscore += pat + 1;
+
+            (long p1, long p2) result;
+            if (p1turn)
+            {
+                result = game(map, universes, pscore, _p2score, pat, _p2at, !p1turn);
+            }
+            else
+            {
+                result = game(map, universes, _p1score, pscore, _p1at, pat, !p1turn);
+            }
+
+            wins.p1 += result.p1 * universes;
+            wins.p2 += result.p2 * universes;
+        }
+
+        return wins;
+    }
+
     /*
         P2
     */
 
     public static long P2((int p1, int p2) players)
     {
-        return -1;
+        // 1313078869 is too low
+        var map = new Dictionary<int, int>();
+        map.Add(3, 1);
+        map.Add(4, 3);
+        map.Add(5, 6);
+        map.Add(6, 7);
+        map.Add(7, 6);
+        map.Add(8, 3);
+        map.Add(9, 1);
+
+        var p1at = players.p1 - 1;
+        var p2at = players.p2 - 1;
+
+        var wins = game(map, 1, 0, 0, p1at, p2at, true);
+
+        return Math.Max(wins.p1, wins.p2);
     }
 }
